4c7382bbcc4ea3da40441fe6a73aa0d218baa481
SYKhayyat <shaulyoelkhayyat@gmail.com>
2026-02-24 06:46:00 -0500

    added comments

--- src/Client.java
+++ src/Client.java
@@ -32,26 +32,26 @@ public class Client {
         ) {
             String userInput;
             String serverResponse;
-            HashMap<Integer, String> packets = new HashMap<>();
-            HashSet<Integer> missingPackets = new HashSet<>();
+            HashMap<Integer, String> packets = new HashMap<>(); // This will hold the packets it receives.
+            HashSet<Integer> missingPackets = new HashSet<>(); // This will hold the number of the packets it did not yet receive.
             userInput = stdIn.readLine();
             while (userInput != null) {
-                userInput += ";";
+                userInput += ";"; // So that the server can differentiate between a regular request and a request to resend packets.
                 requestWriter.println(userInput);// send request to server
                 do {
                     getServerResponse(responseReader, missingPackets, packets);
-                    if (!missingPackets.isEmpty()) {
+                    if (!missingPackets.isEmpty()) { // We have to send a request to get the missing packets.
                         String missing = "";
                         for (int i : missingPackets) {
                             missing += i;
                             missing += " ";
                         }
-                        requestWriter.println(missing);
+                        requestWriter.println(missing); // Sends a s tring with the packet numbers we do not have.
                     } else {
                         break;
                     }
-                } while (!missingPackets.isEmpty());
-                String text = processText(packets);
+                } while (!missingPackets.isEmpty()); // Loop this while we still need packets.
+                String text = processText(packets); // Rearrange the packets, once we have them all.
                 System.out.println(text);
                 userInput = stdIn.readLine();
 
@@ -80,7 +80,7 @@ public class Client {
     private static String processText(HashMap<Integer, String> packets) {
         String text = "";
         for (int i = 0; i < packets.size(); i++) {
-            text += packets.get(i);
+            text += packets.get(i); // Add to the string to be output the packets, ordered by packet number.
         }
         return text;
     }
@@ -92,23 +92,23 @@ public class Client {
         String currentString;
         int totalInt;
         for (int i = serverResponse.length() - 1; i >= 0; i--) {
-            if (serverResponse.charAt(i) == '-'){
+            if (serverResponse.charAt(i) == '-'){ // The numbers after this will always be the total number of packets.
                 totalString = serverResponse.substring(i + 1);
                 serverResponse = serverResponse.substring(0,i);
                 totalInt = Integer.parseInt(totalString);
                 if (missingPackets.isEmpty()){
                     for (int j = 0; j < totalInt; j++) {
-                        missingPackets.add(j);
+                        missingPackets.add(j); // Initialize the missing packets to be full, because, so far, we have not processed a single packet.
                     }
                 }
                 continue;
             }
-            if (serverResponse.charAt(i) == '/'){
+            if (serverResponse.charAt(i) == '/'){// The numbers after this will always be the current number of packets.
                 currentString = serverResponse.substring(i + 1);
                 serverResponse = serverResponse.substring(0,i);
                 currentInt = Integer.parseInt(currentString);
-                missingPackets.remove(currentInt);
-                packets.put(currentInt, serverResponse);
+                missingPackets.remove(currentInt); // The packet is no longer missing.
+                packets.put(currentInt, serverResponse); // Put the received packet in our hashmap of packets.
                 break;
             }
 
